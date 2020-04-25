d7a4dbde91730ca2000c8489d365410a59108a17
Brian Francis De Guzman <deguzmanbrianfrancis@gmail.com>
2020-04-25 15:40:42 -0400

    name banner

---
+++ personalsite-v2/src/components/banner.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+
+const Banner = () => {
+    return(
+        <div className="banner">
+            <div className="container">
+                <div className="row">
+                    <div className="mainText">
+                        Brian Francis De Guzman
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Banner
