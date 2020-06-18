26bece9877caf17580bd476d79b486f6dfced1f6
Brian Francis De Guzman <deguzmanbrianfrancis@gmail.com>
2020-06-18 19:35:00 -0400

    Update subText.js

--- personalsite-v2/src/components/subText.js
+++ personalsite-v2/src/components/subText.js
@@ -5,7 +5,7 @@ const SubText = () => (
 
         I'm a Software Engineer that loves front-end development and 
         technologies. My current adventure has me focusing on <b>C#, 
-        JavaScript, .NET MVC, HTML, CSS, JQuery</b>. 
+        JavaScript, .NET MVC, HTML, CSS,</b> and<b> JQuery</b>. 
         I'm currently teaching myself the basics of modern JavaScript 
         frameworks with <b>Angular and React</b> so watch my GitHub for an upcoming 
         project. I'm constantly tinkering and love how programming allows me
