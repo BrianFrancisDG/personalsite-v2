dd505273e2158a89d970011f1ffd5e54608665f0
Brian Francis De Guzman <deguzmanbrianfrancis@gmail.com>
2020-06-18 19:27:56 -0400

    Proper Helmet Implementation

--- personalsite-v2/src/pages/index.js
+++ personalsite-v2/src/pages/index.js
@@ -1,5 +1,6 @@
-import React from "react"
-import "../styles/styles.scss"
+import React from "react";
+import "../styles/styles.scss";
+import { Helmet } from "react-helmet";
 
 
 //Import Components
@@ -10,6 +11,10 @@ import Buttons from "../components/buttons"
 
 const IndexPage = () => (
   <div className="grid">
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Hi, I'm Brian! | Brian Francis De Guzman</title>
+    </Helmet>
     <Header />
     <MainText />
     <SubText />
