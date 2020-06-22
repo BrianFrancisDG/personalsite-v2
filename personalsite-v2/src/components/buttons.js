12a6dcf70f66989e2ada44e297c997d8f1d863e5
Brian Francis De Guzman <deguzmanbrianfrancis@gmail.com>
2020-06-21 22:14:50 -0400

    update resume link

--- personalsite-v2/src/components/buttons.js
+++ personalsite-v2/src/components/buttons.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Buttons = () => (
     <div className="buttons">
-        <a className="btn" href="https://drive.google.com/file/d/1IjASMC1H-pATphnMWM1ZG04Pl-xwEMAS/view">My Resume</a>
+        <a className="btn" href="https://drive.google.com/file/d/1bVJgmMCPU0NHKX2yRFDAL3Sciqtcvwck/view">My Resume</a>
         <a className="btn" href="mailto:deguzmanbrianfrancis@gmail.com">Contact Me</a>
     </div>
 )
